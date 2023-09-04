ab9095e9ebfdc2e28b60c6c317abcadd9db25d69
jhr <weixinyii386@163.com>
2023-09-04 09:38:06 +0800

    update

---
+++ jhr/view.cpp
@@ -0,0 +1,12 @@
+//
+// Created by jhr on 2023/9/1.
+//
+#include "pcl/io/io.h"
+#include "pcl/io/pcd_io.h"
+#include "pcl/visualization/pcl_visualizer.h"
+
+using namespace pcl
+
+int main(int argc, char **argv) {
+    PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::)
+}
